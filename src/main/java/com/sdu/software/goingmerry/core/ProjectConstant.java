7e24c8357c02286ffa3a1836d7e9e588589cc1e8
liyp <liyp@primeton.com>
2020-04-13 16:57:28 +0800

    Update: 少提交一个类 :bug:

--- src/main/java/com/sdu/software/goingmerry/core/ProjectConstant.java
+++ src/main/java/com/sdu/software/goingmerry/core/ProjectConstant.java
@@ -30,6 +30,10 @@ public final class ProjectConstant {
      */
     public static final String CONTROLLER_PACKAGE = BASE_PACKAGE + ".controller";
     /**
+     *  api 版本
+     */
+    public static final String API_VERSION = "v1";
+    /**
      *  Mapper插件基础接口的完全限定名
      */
     public static final String MAPPER_INTERFACE_REFERENCE = BASE_PACKAGE + ".core.Mapper";
